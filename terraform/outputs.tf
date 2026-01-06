ce8637ef401be3561b6620472239c08512b5c35c
Hamza Hrnjic <hamzahrnjic887@gmail.com>
2026-01-06 23:27:42 +0100

    IAACD Implement everything

---
+++ terraform/outputs.tf
@@ -0,0 +1,19 @@
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.app_server.id
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.app_server.public_ip
+}
+
+output "instance_public_dns" {
+  description = "Public DNS name of the EC2 instance"
+  value       = aws_instance.app_server.public_dns
+}
+
+output "app_url" {
+  description = "URL to access the application"
+  value       = "http://${aws_instance.app_server.public_ip}:3000"
+}
